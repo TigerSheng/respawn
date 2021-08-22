6ecd16ad08f3497fccbbbcacf5837d0b724859ec
Marc <merih.atasoy@gmail.com>
2021-08-22 18:13:12 -0400

    footer work

--- appstatic/src/app/app.module.ts
+++ appstatic/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingModule } from './landing/landing.module';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
